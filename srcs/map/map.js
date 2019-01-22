81c64f48fb989a880feefab1cf383bfe7336d99a
Frisly <34254858+frisly@users.noreply.github.com>
2019-01-21 20:09:16 -0500

    Create map.js

---
+++ srcs/map/map.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta charset='utf-8' />
+                <title>Map v4</title>
+                <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+                <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.js'></script>
+                <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' rel='stylesheet' />
+                <link href="style.css" rel="stylesheet" />
+                <script src="/srcs/map/map.js" type="text/javascript"></script>
+        </head>
+
+        <body>
+                <header>
+                        <p></p>
+                </header>
+
+
+                <div id="map"></div>
+
+
+                <footer>
+                        <a href="#">
+                                Get Involved
+                        </a>
+                        <a href="https://ccaej.org/" target="_blank">
+                                CCAEJ
+                        </a>
+                        <a href="https://www.facebook.com/MyGenerationSC/" target="_blank">
+                                MyGen
+                        </a>
+                </footer>
+        </body>
+</html>
